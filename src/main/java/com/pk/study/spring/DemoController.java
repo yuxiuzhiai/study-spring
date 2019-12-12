0789513de95616f8842af08f4ae715c6cb86adc7
pengkai <pengkai@didiglobal.com>
2019-12-13 01:09:08 +0800

    handler

--- src/main/java/com/pk/study/spring/DemoController.java
+++ src/main/java/com/pk/study/spring/DemoController.java
@@ -1,9 +1,6 @@
 package com.pk.study.spring;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -17,6 +14,14 @@ public class DemoController {
     @RequestMapping("/")
     public String home() {
         return "hello,world";
+    }
+
+    @GetMapping("/userInfo")
+    public UserInfo userInfo(@RequestParam String a, @RequestParam String b) {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setA(a);
+        userInfo.setB(b);
+        return userInfo;
     }
 
     @PostMapping(value = "/file")
